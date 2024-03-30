fe96f69d569eacf9599ffa6a81af815a31159636
Prakashchand Choudhary <89769921+prakash279@users.noreply.github.com>
2024-03-30 23:19:02 +0530

    Add files via upload
    Adding remaining files

---
+++ FRONT-END CODES/files/script.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById('user-id');
+const searchButton = document.querySelector('button');
+
+searchButton.addEventListener('click', () => {
+  const userId = searchInput.value;
+
+  // TODO: Implement search logic here
+});
