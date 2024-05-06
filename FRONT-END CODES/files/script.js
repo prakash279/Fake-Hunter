fe8b7c4fb34fe19375722604332c547a1ed70e5d
PRAKASHCHAND CHOUDHARY <89769921+prakash279@users.noreply.github.com>
2024-05-07 00:37:59 +0530

    Removing not needed files which previously were used for hackathon and making code readible

--- FRONT-END CODES/files/script.js
+++
@@ -1,8 +0,0 @@
-const searchInput = document.getElementById('user-id');
-const searchButton = document.querySelector('button');
-
-searchButton.addEventListener('click', () => {
-  const userId = searchInput.value;
-
-  // TODO: Implement search logic here
-});
